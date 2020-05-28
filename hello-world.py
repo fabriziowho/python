9437d384aa521e6dc68c9b4496efbcd7a2a77144
Fabrizio Chiavari <fabrizio.chiavari@gmail.com>
2020-05-28 11:14:27 +0200

    Nuovo hello world con i lab

--- hello-world.py
+++ hello-world.py
@@ -1,1 +1,20 @@
-print("Hello World")
+def HelloWorld():
+    print("Hello World")
+
+HelloWorld()
+
+def Greeting(name):
+    print("Hi " + name + "!")
+
+Greeting("Fabrizio")
+
+def sum(int1,int2):
+    print(int1+int2)
+
+sum(5,3)
+
+def returnAdd(num1,num2):
+    return (num1+num2)
+
+sum= returnAdd(23,45)
+print (sum)
